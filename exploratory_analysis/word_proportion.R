83bb8c35d9a051bc1fbe3c2182e783789da87e8d
dannyfriar <dannyfriar@hotmail.co.uk>
2017-06-09 17:27:18 +0100

    Toy RL implementation

---
+++ exploratory_analysis/word_proportion.R
@@ -0,0 +1,28 @@
+library(data.table)
+library(ggplot2)
+library(scales)
+
+prop <- read.csv('../data/proportion_companies.csv', header=TRUE)
+prop <- prop[prop$is_bad_url == 0, ]
+
+# Plot the percentage of company names
+g1 <- ggplot(data = prop, aes(x=name_prop)) + geom_histogram(color='firebrick', fill='lightblue', bins=20)
+g1 <- g1 + labs(x = '% Company Names in Page', y = "") + scale_x_continuous(labels = percent, breaks=seq(0, 0.01, 0.002))
+ggsave(file="../figures/our_work/name_prop.png", g1)
+g1
+
+# Plot the percentage of keywords
+g2 <- ggplot(data = prop, aes(x=vert_desc_prop)) + geom_histogram(color='firebrick', fill='lightblue', bins=20)
+g2 <- g2 + labs(x = '% Vertical Keywords in Page', y = "") + scale_x_continuous(labels = percent)
+ggsave(file="../figures/our_work/prop_hist.png", g2)
+g2
+
+prop.filtered <- prop[prop$vert_desc_prop <= 0.1 & prop$name_prop <= 0.005, ]
+g3 <- ggplot(data=prop.filtered, aes(x=vert_desc_prop, y=name_prop))
+g3 <- g3 + geom_point(shape=16, size=2, alpha=.7, show.legend=FALSE, color='firebrick')
+g3 <- g3 + labs(x='% Vertical Keywords in Page', y='% Company Names in Page')
+g3 <- g3 + scale_x_continuous(labels = percent) + scale_y_continuous(labels = percent) + theme_minimal()
+ggsave(file="../figures/our_work/comp_scatter.png", g3)
+g3
+
+cor(prop$vert_desc_prop, prop$name_prop)
