f43d8eada8e36707bc0a47cb0d96a3e2c1e09234
dannyfriar <dannyfriar@hotmail.co.uk>
2017-07-08 15:30:21 +0100

    Correcting value function results

--- rl_toy_implementation/linear_dqn.py
+++ rl_toy_implementation/linear_dqn.py
@@ -216,7 +216,7 @@ def main():
 			# Test URLs
 			test_urls = random.sample(url_set, 5000)
 			# pd.DataFrame.from_dict({'url':test_urls}).to_csv("data/random_url_sample.csv", index=False)
-			test_urls = pd.read_csv("data/random_test_url_sample.csv")['url'].tolist()
+			test_urls = pd.read_csv("data/random_url_sample.csv")['url'].tolist()
 			state_array = build_url_feature_matrix(count_vec, test_urls, revisit, found_rewards)
 			v = sess.run(agent.v, feed_dict={agent.state: state_array}).reshape(-1).tolist()
 			pd.DataFrame.from_dict({'url':test_urls, 'value':v}).to_csv(test_value_files, index=False)
