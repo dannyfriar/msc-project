e87f4ee4eb0619ed5644516493ffd0997d9c131f
dannyfriar <dannyfriar@hotmail.co.uk>
2017-07-08 15:54:06 +0100

    Updating test script

--- rl_toy_implementation/test_stuff.py
+++ rl_toy_implementation/test_stuff.py
@@ -5,4 +5,7 @@ import pandas as pd
 actual_list = pd.read_csv("results/actual_value_revisit.csv")["url"].tolist()
 pred_list = pd.read_csv("results/linear_dqn_results/test_value_revisit.csv")["url"].tolist()
 
-print(len(set(actual_list).intersection(set(pred_list))))
+print(sorted(actual_list)[:10])
+print(sorted(pred_list)[:10])
+
+# print(len(set(actual_list).intersection(set(pred_list))))
