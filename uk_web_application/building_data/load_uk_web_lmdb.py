c377fb5b1d55bd2b88bc2c80ac97396384b5decf
dannyfriar <dannyfriar@hotmail.co.uk>
2017-08-10 16:09:18 +0100

    4 runs linear, 3 runs embedding

---
+++ uk_web_application/building_data/load_uk_web_lmdb.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+import asyncio
+import gzip
+import collections
+import os
+import sys
+import ujson as json
+import zstd
+import capnp
+import argparse
+import random
+import uuid
+import signal
+import string
+import lmdb
+import glob
+import time
+import logging
+import cython
+
+from evolutionai import webpage_capnp
+from evolutionai import StorageEngine
+
+from nats_stream.aio.client import StreamClient
+from nats_stream.aio.publisher import Publisher
+from nats_stream.aio.client import StreamClient, Msg
+from nats_stream.aio.subscriber import Subscriber
+
+logging.basicConfig(filename='log_files/load_uk_web.log', level=logging.DEBUG)
+dctx = zstd.ZstdDecompressor()
+
+whitelist = ["net", "org", "com", "uk"]
+tld_counter = collections.Counter()
+
+def progress_bar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+    sys.stdout.write("\rPercent complete: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+
+def stream_file(path, topic, client_id):
+	global env, dctx
+	cctx = zstd.ZstdCompressor(level=5, write_content_size=True)
+	f = gzip.open(path, "rt")
+	data_list = []
+
+	for count, line in enumerate(f):
+		# progress_bar(count, 3000)
+		if line[0] != "{":
+			continue
+		j = json.loads(line)
+		if "WARC-Target-URI" not in j["Envelope"]["WARC-Header-Metadata"]:
+			continue
+		if "HTTP-Response-Metadata" not in j['Envelope']['Payload-Metadata']:
+			continue
+		if "HTML-Metadata" not in j['Envelope']['Payload-Metadata']["HTTP-Response-Metadata"]:
+			continue
+		page = webpage_capnp.Page.new_message()
+		page.url = j["Envelope"]["WARC-Header-Metadata"]["WARC-Target-URI"]
+		tld_counter[page.url.split("/")[2].split(".")[-1]] += 1
+		if not page.url.split("/")[2].endswith(tuple(whitelist)):
+			continue
+		page.title = j['Envelope']['Payload-Metadata']["HTTP-Response-Metadata"]['HTML-Metadata'] \
+			.get('Head', {}).get('Title', "")
+		links = j['Envelope']['Payload-Metadata']["HTTP-Response-Metadata"]['HTML-Metadata'].get('Links', [])
+		clinks = page.init("links", len(links))
+
+		for i, d in enumerate(links):
+			l = clinks[i]
+			l.url = links[i].get("url", "")
+			l.text = links[i].get("text", "")
+			l.title = links[i].get("title", "")
+
+		payload = page.to_bytes()
+		compressed = cctx.compress(payload)
+		data_list.append((page.url[:500].encode("UTF-8"), compressed))
+
+	with env.begin(write=True) as txn:
+		txn.cursor().putmulti(data_list)
+
+
+def main():
+	client_id = str(uuid.uuid4())[:8]
+	parser = argparse.ArgumentParser()
+	parser.add_argument("dir")
+	parser.add_argument("db_path")
+	# parser.add_argument("num_files")
+	args = parser.parse_args()
+
+	global env
+	env = lmdb.open(args.db_path, map_size=1024**4)
+	file_list = sorted(os.listdir(args.dir))
+	file_list = [l for l in file_list if "gz" in l]
+	file_list = file_list[35000:]
+	print("Running for directory {} with {} files".format(args.dir, len(file_list)))
+
+	print_iters = 100
+	t0 = time.time()
+	for idx, file in enumerate(file_list):
+		filename = args.dir+ "/" + file
+		progress_bar(idx+1, len(file_list))
+		try:
+			logging.info('Running for file {}...'.format(file))
+			stream_file(filename, "links", client_id)
+			logging.info('Completed file {}, time elapsed = {}.'.format(file, time.time()-t0))
+		except EOFError:
+			print("\n EOF error on file {}.".format(file))
+			logging.info("Error for file {}.".format(file))
+			pass
+		if idx % print_iters == 0:
+			print("\nRun for {} files, time elapsed = {}".format(idx+1, time.time()-t0))
+	print("\n")
+
+
+if __name__ == "__main__":
+	main()
---
+++ uk_web_application/random_crawler.py
@@ -0,0 +1,166 @@
+import os
+import sys
+import re
+import csv
+import time
+import random
+import argparse
+import ahocorasick
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from collections import OrderedDict
+
+from evolutionai import StorageEngine
+storage = StorageEngine("/nvme/uk_web/")
+
+RESULTS_FOLDER = "results/random_results/"
+
+##-----------------------------------------------------------
+##-------- Miscellaneous Functions --------------------------
+def progress_bar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+    sys.stdout.write("\rPercent complete: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+
+def init_automaton(string_list):
+	"""Make Aho-Corasick automaton from a list of strings"""
+	A = ahocorasick.Automaton()
+	for idx, s in enumerate(string_list):
+		A.add_word(s, (idx, s))
+	return A
+
+def check_strings(A, search_list, string_to_search):
+	"""Aho Corasick algorithm, return boolean list of strings within longer string"""
+	index_list = []
+	for item in A.iter(string_to_search):
+		index_list.append(item[1][0])
+
+	output_list = np.array([0] * len(search_list))
+	output_list[index_list] = 1
+	return output_list.tolist()
+
+##-----------------------------------------------------------
+##-------- Get links from LMDB data functions ---------------
+def get_list_of_links(url, s=storage):
+	"""Use the LMDB database to get a list of links for a given URL"""
+	try:
+		page = s.get_page(url)
+		if page is None:
+			page = s.get_page(url+"/")
+		if page is None:
+			page = s.get_page("www."+url)
+		if page is None:
+			page = s.get_page("www."+url+"/")
+		if page is None:
+			return []
+	except (UnicodeError, ValueError):
+		return []
+	try:
+		link_list = [l.url.replace("http://", "").replace("https://", "") for l in page.links if l.url[:4] == "http"]
+		link_list = link_list + [l.replace("www.", "") for l in link_list]
+	except UnicodeDecodeError:
+		return []
+	return link_list
+
+##-----------------------------------------------------------
+##-------- RL Functions -------------------------------------
+def get_random_url(url_list, recent_urls):
+	"""Get random url that is not in list of recent URLs"""
+	url = random.choice(url_list)
+	while url in recent_urls:
+		url = random.choice(url_list)
+	return url
+
+def get_reward(url, A_company, company_urls):
+	"""Return 1 if company URL, 0 otherwise"""
+	idx_list = check_strings(A_company, company_urls, url)
+	if sum(idx_list) > 0:
+		reward_url_idx = np.nonzero(idx_list)[0][0]
+		return 1, reward_url_idx
+	return 0, None
+
+##-----------------------------------------------------------
+##-----------------------------------------------------------
+def main():
+	##-------------------- Read in data - reward URLs and all URLs in the state space
+	links_df = pd.read_csv("../rl_toy_implementation/new_data/links_dataframe.csv")
+	rm_list = ['aarp.org', 'akc.org', 'alcon.com', 'lincoln.com', 'orlakiely.com', 
+	'red.com', 'ef.com', 'ozarksfirst.com']
+	links_df['domain'] = links_df.domain.str.replace("www.", "")
+	links_df = links_df[~links_df['domain'].isin(rm_list)]
+	reward_urls = links_df[links_df['type']=='company-url']['url']
+	reward_urls = [l.replace("www.", "") for l in reward_urls]
+	A_company = init_automaton(reward_urls)  # Aho-corasick automaton
+	A_company.make_automaton()
+
+	# start_url_list = links_df[links_df['type']=='first-hop-link']['url'].tolist()
+	start_url_list = links_df['url'].tolist()
+	start_url_list = [l for l in start_url_list if ".uk" in l]
+
+	# Set paths
+	all_urls_file = RESULTS_FOLDER + "all_urls.csv"
+	
+	##-------------------- Random crawling
+	cycle_freq = 50
+	number_crawls = 200000
+	print_freq = 1000
+	term_steps = 20
+
+	# To store data
+	pages_crawled = 0; total_reward = 0; terminal_states = 0; num_steps = 0
+	recent_urls = []; reward_pages = []
+	reward_domain_set = set()
+
+	if os.path.isfile(all_urls_file):
+		os.remove(all_urls_file)
+
+	while num_steps < number_crawls:
+		url = get_random_url(start_url_list, recent_urls)
+		steps_without_terminating = 0
+
+		while num_steps < number_crawls:
+			num_steps += 1
+
+			# Track progress
+			progress_bar(num_steps, number_crawls)
+			if num_steps % print_freq == 0:
+				print("\nCrawled {} pages, total reward = {}, # terminal states = {}, remaining rewards = {}"\
+					.format(pages_crawled, total_reward, terminal_states, len(reward_urls)))
+
+			# Keep track of recent URLs (to avoid loops)
+			recent_urls.append(url)
+			if len(recent_urls) > cycle_freq:
+				recent_urls = recent_urls[-cycle_freq:]
+
+			# Get rewards
+			r, reward_url_idx = get_reward(url, A_company, reward_urls)
+			pages_crawled += 1
+			total_reward += r
+			with open(all_urls_file, "a") as csv_file:
+				writer = csv.writer(csv_file, delimiter=',')
+				writer.writerow([url, r])
+
+			if r > 0:
+				break
+
+			# List of next possible URLs 
+			link_list = get_list_of_links(url)
+			link_list = [l for l in link_list if ".uk" in l]
+			link_list = list(set(link_list) - set(recent_urls))
+
+			# Choose next URL from list
+			if len(link_list) == 0:
+				terminal_states += 1
+				break
+			steps_without_terminating += 1
+			if steps_without_terminating >= term_steps:
+				break
+			url = random.choice(link_list)
+
+
+if __name__ == "__main__":
+	main()
